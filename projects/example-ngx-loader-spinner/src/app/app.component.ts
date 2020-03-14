d2e0e152a0b128f412d4ede5a4d2a595d5066d8b
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2020-03-15 02:37:51 +0600

    use ngx-loader to example application

--- projects/example-ngx-loader-spinner/src/app/app.component.ts
+++ projects/example-ngx-loader-spinner/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxLoaderSpinnerService } from 'ngx-loader-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'example-ngx-loader-spinner';
+export class AppComponent implements OnInit {
+  constructor(
+    private loader: NgxLoaderSpinnerService
+  ) {
+  }
+
+  ngOnInit() {
+    this.showLoader();
+  }
+
+  showLoader() {
+    this.loader.show();
+    setTimeout(() => {
+      this.loader.hide();
+    }, 5000);
+  }
 }
--- projects/example-ngx-loader-spinner/src/app/app.module.ts
+++ projects/example-ngx-loader-spinner/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxLoaderSpinnerModule } from 'ngx-loader-spinner';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxLoaderSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
